d02514f08ca5a6c1d9f6a32d13c58b15afe26d38
LittleWing85 <mail@alexandra-jaeger.net>
2023-08-12 18:03:28 +0200

    saving of current status before integrating redux toolkit

--- client/src/common/loginLogoutRegister/Login.js
+++ client/src/common/loginLogoutRegister/Login.js
@@ -1,3 +1,71 @@
-export default function Login() {
-    return <p>login here soon</p>;
+import { useState } from "react";
+import "./loginLogoutRegister.css";
+
+export default function Login({ toggleLoggedIn, onRegisterClick }) {
+    const [showLoginErrorMessage, setShowLoginErrorMessage] = useState(false);
+
+    function onSubmitLogin(event) {
+        event.preventDefault();
+        const loginData = {
+            email: event.target.email.value,
+            password: event.target.password.value,
+        };
+        fetch("/api/login", {
+            method: "POST",
+            body: JSON.stringify(loginData),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((response) => response.json())
+            .then((foundUser) => {
+                if (foundUser) {
+                    setShowLoginErrorMessage(false);
+                    toggleLoggedIn();
+                    return;
+                }
+                setShowLoginErrorMessage(true);
+            })
+            .catch((error) => console.log(error));
+    }
+
+    function switchToRegistration() {
+        onRegisterClick();
+    }
+
+    return (
+        <div>
+            {showLoginErrorMessage && (
+                <p className="errorMessage">
+                    Wrong credentials or you haven&apos;t registered yet.
+                </p>
+            )}
+            <form className="flexVertically " onSubmit={onSubmitLogin}>
+                <label htmlFor="email">Email address</label>
+                <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    required
+                    placeholder="Email"
+                />
+                <label htmlFor="password">Password</label>
+                <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    required
+                    placeholder="Password"
+                />
+                <button>Login</button>
+            </form>
+            <p className="topSpace">
+                No account yet? Click{" "}
+                <span className="clickHere" onClick={switchToRegistration}>
+                    here
+                </span>{" "}
+                to register!
+            </p>
+        </div>
+    );
 }
