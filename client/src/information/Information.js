ce0644f62d3ed1cf9afa97435578637762839d2d
LittleWing85 <mail@alexandra-jaeger.net>
2025-07-29 22:51:21 +0200

    basic structure for information about nursing added

---
+++ client/src/information/Information.js
@@ -0,0 +1,10 @@
+export default function Information() {
+    return (
+        <section>
+            <p>
+                I will add wonderful information about how to nurse cute babies
+                here later!
+            </p>
+        </section>
+    );
+}
