db8aff5ac349c1771da0591d104c2be01ba82623
littlewing <mail@alexandra-jaeger.net>
2025-09-09 15:48:03 +0200

    Update: From React Router v5 to v6 for root element and first level pages completed

--- client/src/documentationTool/DocumentationTool.js
+++ client/src/documentationTool/DocumentationTool.js
@@ -1,38 +1,13 @@
-import { NavLink, Route, BrowserRouter } from "react-router-dom";
 import WhosNext from "./WhosNext.js";
 import NewLitter from "./NewLitter.js";
 import LitterOverview from "./LitterOverview.js";
-/*https://www.codecademy.com/learn/learn-react-router
-https://www.youtube.com/watch?v=PWi9V9d_Jsc
-https://stackoverflow.com/questions/74991933/navigation-to-multiple-pages-using-react-router-dom
-https://reactrouter.com/start/declarative/routing
-https://www.robinwieruch.de/react-router-nested-routes/
-https://stackoverflow.com/questions/77165891/nested-routes-in-reactjs
-https://gondi-sai.medium.com/building-nested-components-with-child-routes-in-react-react-no-14-c9152db9f8cc*/
+
 export default function DocumentationTool() {
     return (
-        <BrowserRouter>
-            <nav className="navSecondLevel">
-                <NavLink to="/whosNext">Who&apos;s next?</NavLink>
-                <NavLink to="/newLitter">Add new Litter</NavLink>
-                <NavLink to="/litterOverview">Litter overview</NavLink>
-            </nav>
-
-            <div>
-                <section>
-                    <Route path="/whosNext">
-                        <div>
-                            <WhosNext />
-                        </div>
-                    </Route>
-                    <Route path="/newLitter">
-                        <NewLitter />
-                    </Route>
-                    <Route path="/litterOverview">
-                        <LitterOverview />
-                    </Route>
-                </section>
-            </div>
-        </BrowserRouter>
+        <div>
+            <section>
+                <p>Coming Soon!</p>
+            </section>
+        </div>
     );
 }
