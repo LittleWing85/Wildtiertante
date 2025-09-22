2fdf599df4e4dd195bd0395d862ba57746b9a944
littlewing <mail@alexandra-jaeger.net>
2025-09-22 15:02:08 +0200

    Refactoring and bugfix: Routing for logout button replaced by linking and typo in styling for logout button fixed

--- client/src/basics/loginLogoutRegister/LoginLogoutButtonsAndLogout.js
+++ client/src/basics/loginLogoutRegister/LoginLogoutButtonsAndLogout.js
@@ -26,8 +26,6 @@ export default function LoginLogoutButtonsAndLogout() {
         dispatch(logout());
         fetch("/api/logout", {
             method: "POST",
-        }).then(() => {
-            navigate("/");
         });
     }
 
