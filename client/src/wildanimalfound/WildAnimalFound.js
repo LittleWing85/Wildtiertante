ae15d5807c18732fab0b1241419c9f7b2f442357
LittleWing85 <mail@alexandra-jaeger.net>
2025-10-10 23:55:13 +0200

    Content added: Wildtier gefunden

--- client/src/wildanimalfound/WildAnimalFound.js
+++ client/src/wildanimalfound/WildAnimalFound.js
@@ -2,6 +2,100 @@ export default function WildAnimalFound() {
     return (
         <section>
             <h1>Wildtier gefunden, was tun? </h1>
+            <p className="bottomSpace">
+                Nicht alle Wildtiere, die wir bei Ausflügen in der Natur oder in
+                der Stadt antreffen, sind hilfebedürftig &ndash; und auch bei
+                hilfebedürftigen Wildtieren gibt es einiges zu beachten. Ein
+                Wildtier aus der Natur zu entnehmen sollte immer die letzte
+                Möglichkeit sein, da die Tiere dabei großem Stress ausgesetzt
+                werden. Sollte es keine andere Möglichkeit geben, um einem
+                Findling zu helfen, darf die erste Hilfe und jede weitere
+                Versorgung ausschließlich durch sachkundige Hände erfolgen. Nur
+                so hat ein Tier in Not eine Chance, gesund zu werden und
+                auswilderungsfähig zu bleiben.
+            </p>
+            <h1>Anzeichen für Hilfebedürftigkeit</h1>
+            <p>
+                Wenn ihr sicher seid, dass ein Tier ohne menschliche Hilfe keine
+                Chance auf ein Überleben hat, darf es mitgenommen werden. Fragt
+                im Zweifelsfall telefonisch vorab bei Wildtierstationen,
+                wildtierkundigen Tierheimen etc. nach einer Einschätzung. Dabei
+                könnt ihr gleichzeitig abklären, ob euer Findling dort ggf.
+                aufgenommen werden kann. Sichere Anzeichen für
+                Hilfebedürftigkeit sind z.B.:
+            </p>
+            <ul className="topSpaceSmall">
+                <li>
+                    Das Tier liegt auf der Seite und steht nicht auf, auch wenn
+                    ihr sehr nahe kommt
+                </li>
+                <li>Größere Verletzungen</li>
+                <li>
+                    Fliegen haben bereits ihre Eier auf dem Findling abgelegt
+                </li>
+            </ul>
+            <h2>Spezialfall Tierbabies</h2>
+            <p>
+                Babytiere sind besonders empfindlich und keine noch so erfahrene
+                und fachkundige Menschenhand kann das Muttertier ersetzen. Bitte
+                versucht daher unbedingt, Tierbabies zurückzusetzen, falls ihr
+                irgendwo in erreichbarer Nähe das Nest findet und wartet ab, ob
+                das Muttertier zurückkehrt. Bitte sucht die Umgebung vorher nach
+                weiteren Babytieren ab. Wildtiere, die in der Nähe von Menschen
+                leben, sind an unseren Geruch gewöhnt und nehmen ihren Nachwuchs
+                in der Regel auch dann wieder an, wenn er ein wenig nach Mensch
+                riecht. In folgenden Fällen können Tierbabies allerdings nicht
+                in das Nest zurückgesetzt werden, da sie menschliche Hilfe
+                benötigen:
+            </p>
+            <ul className="topSpaceSmall">
+                <li>Die Babies sind kühl oder kalt</li>
+                <li>Bei Verletzungen</li>
+                <li>Fliegen haben bereits ihre Eier auf den Babies abgelegt</li>
+                <li>
+                    Bei Eichhörnchenbabies, die euch hinterherlaufen oder sogar
+                    am Bein hochklettern &#40;in der Regel ist das Muttertier
+                    nicht zum Nachwuchs zurückgekehrt&#41;
+                </li>
+            </ul>
+            <p className="topSpaceSmall">
+                Hilfebedürftige Tierbabies benötigen moderate Wärme &ndash; z.B.
+                Körperwärme oder eine lauwarme Wärmflasche. Auf keinen Fall
+                sollten Wasser, Muttermilchersatz, Tee o.Ä. in die Mäulchen
+                eingegeben oder in einem Napf bereitgestellt werden. Geschwächte
+                Babytiere können nicht richtig abschlucken und dadurch ersticken
+                oder in Wassernäpfen ertrinken. Bitte sucht die Umgebung
+                unbedingt nach weiteren Tierbabies ab, sodass keines
+                zurückbleibt.
+            </p>
+
+            <h2>Spezialfall Nestflüchter</h2>
+            <p className="bottomSpace">
+                Sogenannte Nestflüchter wie Feldhasen und Rehe kommen mit einem
+                vollständigen Fell zur Welt. So können sie bereits ab der Geburt
+                selbständig ihre Körpertemperatur stabil halten. Das Muttertier
+                kommt nur wenige Male am Tag für kurze Zeit zum Säugen vorbei
+                und hält sich ansonsten von seinem Nachwuchs fern, um keine
+                Fressfeinde anzulocken. Falls ihr also ein Rehkitz oder ein
+                Feldhasenbaby alleine antrefft, besteht in den meisten Fällen
+                kein Grund zur Sorge. Ausnahmen sind:
+            </p>
+            <ul className="topSpaceSmall">
+                <li>
+                    Das Muttertier ist mindestens einen halben Tag lang nicht
+                    zum Säugen vorbeigekommen
+                </li>
+                <li>Bei Verletzungen</li>
+                <li>Fliegen haben bereits ihre Eier auf den Babies abgelegt</li>
+            </ul>
+            <h2>Spezialfall Igel</h2>
+            <p className="bottomSpace"></p>
+            <h1>Wie sieht&#39;s mit der Zuständigkeit aus?</h1>
+            <p className="bottomSpace"></p>
+            <h1>Wohin mit unserem Findling?</h1>
+            <p className="bottomSpace"></p>
+            <h1>Sicherheit geht vor!</h1>
+            <p className="bottomSpace"></p>
         </section>
     );
 }
