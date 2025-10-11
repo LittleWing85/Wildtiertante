2858664616250c909d923d15045ac7a56f7e7a12
littlewing <mail@alexandra-jaeger.net>
2025-10-11 21:40:46 +0200

    rework of content: rework of text on about page

--- client/src/about/About.js
+++ client/src/about/About.js
@@ -3,52 +3,62 @@ import { Link } from "react-router-dom";
 export default function About() {
     return (
         <section>
-            <p>
-                Liebe Päppler, <br />
-                <br />
+            <p>Liebe Wildtierfreunde,</p>
+            <p className="topSpace">
                 schön, dass ihr auf meiner Seite gelandet seid!
             </p>
-            <p>
-                Ihr findet <Link to={"/information"}>hier</Link> Informationen
-                zur Jungtieraufzucht von Eichhörnchen, Mäusen und Kaninchen.
+            <p className="topSpaceSmall">
+                Meine große Leidenschaft gilt der Aufzucht von verwaisten
+                Wildtierbabys. Auf dieser Seite stelle ich euch alle Tipps und
+                Infos zur Verfügung, die ich in meinen elf Jahren als Ersatzmama
+                für verwaiste Tierbabies gesammelt habe.
+            </p>
+            <p className="topSpaceSmall">
+                <Link to={"/information"}>Hier</Link> findet ihr ausführliche{" "}
+                <b>Informationen zum Aufziehen von verwaisten Jungtieren</b>.
                 Wichtig: Diese Informationen richten sich ausschließlich an
                 Zieheltern, die bereits über Fachwissen verfügen - z.B.
                 Tierpfleger*innen, TFAs, Tierschutzzentren und Pflegestellen,
-                die bereits Babytiere anderer Tierarten aufgezogen haben.
-                Jungtiere gehören IMMER in die Hände von Fachmenschen, um ihnen
-                eine Überlebenschance zu sichern. NIEMALS sollten unerfahrene
-                Finder Jungtiere alleine aufziehen.
+                die schon Babytiere aufgezogen haben. Jungtiere gehören IMMER in
+                die Hände von Fachmenschen, um ihnen eine Überlebenschance zu
+                sichern. Auf keinen Fall sollten unerfahrene Finder Jungtiere
+                alleine aufziehen.
             </p>
-            <p>
-                Das Herzstück meiner Webseite bildet die{" "}
-                <Link to={"/documentationTool"}>App</Link>, die euch bei den
-                Milchfütterungen aller Tierarten unterstützt. Sie zeigt euch an,
-                welche Tiere als nächstes und in welcher Reihenfolge gefüttert
-                werden müssen, damit ihr den Überblick behaltet, wenn ihr sehr
-                viele Tiere aufzieht und/oder mehrere Ersatzeltern eure Kleinen
-                in Schichten versorgen. Außerdem rechnet sie euch automatisch
-                aus, welche Milchmenge eute Kleinen pro Fütterung ungefähr
-                bekommen sollten und überwacht die Gesamtmilchmenge pro Tag.
-                Schaut euch die App an - sie hält noch tolle und hilfreiche
-                weitere Funktionen für euch bereit!
+            <p className="topSpaceSmall">
+                Damit ihr beim Päppeln eurer Schützlinge den Überblick behaltet,
+                habe ich ein{" "}
+                <b>
+                    spezielles <Link to={"/documentationTool"}>Tool</Link>
+                </b>{" "}
+                entwickelt. Es zeigt euch an, welche Tierbabys als nächstes und
+                in welcher Reihenfolge gefüttert werden müssen. Das ist
+                besonders nützlich, wenn ihr sehr viele Tiere aufzieht und/oder
+                mehrere Ersatzeltern eure Kleinen in Schichten versorgen.
+                Außerdem rechnet das Tool anhand von Körpergewicht und Alter
+                automatisch aus, welche Milchmenge eure Kleinen pro Fütterung
+                ungefähr bekommen sollten. So braucht ihr nicht bei jeder
+                Fütterung von vorne losrechnen. Schaut euch das{" "}
+                <Link to={"/documentationTool"}>Tool</Link> einfach an - es hält
+                weitere tolle und hilfreiche Funktionen für euch bereit!
             </p>
-            <p>
-                Noch ein paar Infos zu mir: Ich habe ehrenamtlich und
-                hauptberuflich 11 Jahre Erfahrung in der Aufzucht von Jungtieren
-                in verschiedenen Stationen in Norddeutschland gesammelt,
-                besonders kleine und empfindliche Flaschenkinder ehrenamtlich zu
-                Hause als private Pflegestelle aufgezogen und zahlreiche
-                Seminare zum Thema Jungtieraufzucht besucht. Bei mir zu Hause
-                leben außerdem zwei Kaninchen, die ich aus dem Tierschutz
-                übernommen habe.
+            <p className="topSpaceSmall">
+                Nun noch <b>ein paar Infos zu mir</b>: Mein Wissen stammt von
+                verschiedenen, auf Wildtiere spezialisierte Seminare und den
+                Erfahrungen aus elf Jahren, in denen ich ehrenamtlich und
+                hauptberuflich in verschiedenen Wildtierstationen
+                Norddeutschlands gearbeitet habe. Dabei durfte ich Finder von
+                Wildtieren telefonisch beraten, erste Hilfe für Neuankömmlinge
+                leisten und verwaiste Tierbabies aufziehen. Einige besonders
+                kleine und empfindliche Flaschenkinder habe ich als
+                ehrenamtliche Pflegestelle zu Hause rund um die Uhr versorgt.
+                Bei mir leben außerdem dauerhaft zwei wunderbare Kaninchen, die
+                ich aus dem Tierschutz adoptiert habe.
             </p>
-            <p>
+            <p className="topSpace">
                 Ich wünsche euch viel Spaß auf meiner Seite und freue mich über
                 euer Feedback, Anregungen, Fragen und Verbesserungsvorschläge!
-                <br />
-                <br />
-                Eure Wildtiertante
             </p>
+            <p className="topSpaceSmall">Eure Wildtiertante</p>
         </section>
     );
 }
