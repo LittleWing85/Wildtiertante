8aedcdf62ed0d27eb9be3fa492e8682d9ae3cb34
LittleWing85 <mail@alexandra-jaeger.net>
2025-10-20 18:27:37 +0200

    refactoring: code that checks if user is logged in moved to new parent component

--- client/src/documentationTool/DocumentationTool.js
+++ client/src/documentationTool/DocumentationTool.js
@@ -1,6 +1,28 @@
-import { Outlet, NavLink } from "react-router-dom";
+// This component renders the second level nav and checks if user is logged in
+
+import { useEffect } from "react";
+import { Outlet, NavLink, useNavigate } from "react-router-dom";
 
 export default function DocumentationTool() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        fetch("/api/litterOverview")
+            .then((response) => response.json())
+            .then((data) => {
+                if (!data) {
+                    navigate("/");
+                    alert("Please log in first to use this functionality.");
+                    return;
+                }
+                if (data.length === 0) {
+                    alert("Login successful, you have currently no animals");
+                    return;
+                }
+                alert("Login successful");
+            });
+    }, [navigate]);
+
     return (
         <section>
             <nav className="nav2">
