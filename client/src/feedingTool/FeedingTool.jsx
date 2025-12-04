a344718e710ff043e41437cfa43360f48cb34056
Dein Name <mail@alexandra-jaeger.net>
2025-12-04 11:57:48 +0100

    Rework: redundant code deleted

--- client/src/feedingTool/FeedingTool.jsx
+++ client/src/feedingTool/FeedingTool.jsx
@@ -1,24 +1,9 @@
-//This component renders the second level nav and checks if the user is logged in.
-//It also has an Outlet so the content chosen by a user via second level nav will be rendered.
+//This component renders the second level nav. It also has an Outlet
+// so the content chosen by a user via second level nav will be rendered.
 
-import { useEffect } from "react";
-import { Outlet, NavLink, useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { Outlet, NavLink } from "react-router-dom";
 
 export default function FeedingTool() {
-    const navigate = useNavigate();
-    const logged = useSelector((state) => state.loggedin.value);
-    useEffect(() => {
-        if (!logged) {
-            navigate("/signIn", {
-                state: {
-                    message:
-                        "Bitte melde dich an, um das Fütterungstool zu verwenden.",
-                },
-            });
-        }
-    }, [logged, navigate]);
-
     return (
         <section>
             <nav className="nav2">
