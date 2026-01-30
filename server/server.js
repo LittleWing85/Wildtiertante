01695123ff722f26067ea2dae9532352196b63ce
Dein Name <mail@alexandra-jaeger.net>
2026-01-30 21:57:20 +0100

    Rework backend: extraction of route nextFeedings

--- server/server.js
+++ server/server.js
@@ -7,12 +7,8 @@ import newLitterRouter from "./protectedRoutes/newLitter.js";
 import litterOverviewRouter from "./protectedRoutes/litterOverview.js";
 import unfedLittersRouter from "./protectedRoutes/unfedLitters.js";
 import routerFeedingData from "./protectedRoutes/feedingData.js";
-import {
-    createUser,
-    createFeedingEntry,
-    login,
-    getAllFeedings,
-} from "./protectedRoutes/protectedRoutesDb.js";
+import getallFeedingsRouter from "./protectedRoutes/nextFeedings.js";
+import { createUser, login } from "./protectedRoutes/protectedRoutesDb.js";
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
@@ -104,79 +100,7 @@ app.use("/api/newLitter", newLitterRouter);
 api.use("/api/feedingData", routerFeedingData);
 app.use("/api/litterOverview", litterOverviewRouter);
 app.use("/api/unfedLitters", unfedLittersRouter);
-
-/* Gets litters that have been fed at least once 
-and maps the time the litters have been feed last to the time when the next feeding should happen */
-app.get(
-    "/api/nextFeedings",
-    requireLogin,
-    wrap(async (request, response) => {
-        const currentUser = request.session.user_id;
-        const allFeedings = await getAllFeedings(currentUser); // all data about feedings that have already happened
-        const lastFeedings = []; // data about the last time each litter was fed
-        for (const item of allFeedings) {
-            if (
-                !lastFeedings
-                    .map((item) => item.litter_id)
-                    .includes(item.litter_id)
-            ) {
-                lastFeedings.push(item);
-            } else {
-                const found = lastFeedings.find(
-                    (x) => x.litter_id === item.litter_id,
-                );
-                if (item.feeding_id > found.feeding_id) {
-                    const index = lastFeedings
-                        .map((el) => el.litter_id)
-                        .indexOf(found.litter_id);
-                    lastFeedings[index] = item;
-                }
-            }
-        }
-
-        /* maps time of last feeding for each litter to the time each litter should be fed next */
-        const nextFeedings = lastFeedings
-            .map(function (feeding) {
-                const arrayFeedingTimes = feeding.feedingslots; // array with all daily feeding slots
-                const lastFeedingTime = feeding.feedingslot; // time of the last feeding
-                const currentIndex = arrayFeedingTimes.indexOf(lastFeedingTime); // position of the last feeding time in the array with all feeding slots
-                const nextIndex = (currentIndex + 1) % arrayFeedingTimes.length; // position of the next feeding time in the array with all feeding slots
-                const nextFeedingTime = arrayFeedingTimes[nextIndex]; // time of the next feeding
-                //Puts together the time of the next feeding with the date from the last feeding
-
-                //If nextIndex is 0, the next feeding happens on the next day so JS has to add 1 to the day
-                if (nextIndex === 0) {
-                    let nextFeedingDateAndTime = new Date(
-                        feeding.feedingdate + "T" + nextFeedingTime + ".000Z",
-                    );
-                    nextFeedingDateAndTime.setDate(
-                        nextFeedingDateAndTime.getDate() + 1,
-                    );
-                    return {
-                        ...feeding,
-                        nextFeedingTime: nextFeedingTime,
-                        nextFeedingDateAndTime: nextFeedingDateAndTime,
-                    };
-                }
-                let nextFeedingDateAndTime = new Date(
-                    feeding.feedingdate + "T" + nextFeedingTime + ".000Z",
-                );
-                return {
-                    ...feeding,
-                    nextFeedingTime: nextFeedingTime,
-                    nextFeedingDateAndTime: nextFeedingDateAndTime,
-                };
-            })
-            .sort((a, b) => {
-                if (a.nextFeedingDateAndTime > b.nextFeedingDateAndTime) {
-                    return 1;
-                }
-                return -1;
-            });
-        // allFeedings is an array that contains an object for each litter; each object includes the data of the last feeding, the litter and the next feeding time
-        response.json(nextFeedings);
-    }),
-);
+app.use("/api/nextFeedings", getallFeedingsRouter);
 
 // FALLBACK
 app.get("*", function (request, response) {
