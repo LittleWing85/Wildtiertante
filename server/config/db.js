7a7790210e70a1e5448beca0356b63bb9cfc19f4
Dein Name <mail@alexandra-jaeger.net>
2026-02-13 17:22:50 +0100

    Backend refactoring: config/db.js finalised

--- server/config/db.js
+++ server/config/db.js
@@ -1,35 +1,47 @@
 import { Pool } from "pg";
-import fs from "fs";
-import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 let dbConfig;
 
-if (!process.env.DATABASE_URL) {
-    const secretsPath = path.resolve(process.cwd(), "server/secrets.json");
-    const secretsRaw = fs.readFileSync(secretsPath);
-    const secrets = JSON.parse(secretsRaw);
+// FOR DEPLOYMENT
+if (process.env.DATABASE_URL) {
+    dbConfig = {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false }, // Careful! Depending on Provider + Security issues
+        max: 10,
+        idleTimeoutMillis: 30000,
+        connectionTimeoutMillis: 2000,
+    };
+}
 
-    const { DATABASE_USER, DATABASE_PASSWORD } = secrets;
-    const DATABASE_NAME = "wildtiertante";
+// FOR LOCAL DEVELOPMENT
+else {
+    const {
+        DATABASE_USER,
+        DATABASE_PASSWORD,
+        DATABASE_NAME = "wildtiertante",
+    } = process.env;
+
+    if (!DATABASE_USER || !DATABASE_PASSWORD) {
+        throw new Error(
+            "Missing database environment variables. Please set DATABASE_USER and DATABASE_PASSWORD in the .env file.",
+        );
+    }
+
     dbConfig = {
         user: DATABASE_USER,
         password: DATABASE_PASSWORD,
         host: "localhost",
         port: 5432,
         database: DATABASE_NAME,
-    };
-} else {
-    dbConfig = {
-        connectionString: process.env.DATABASE_URL,
-        ssl: { rejectUnauthorized: false }, // change for deployment
+        max: 10,
+        idleTimeoutMillis: 30000,
+        connectionTimeoutMillis: 2000,
     };
 }
 
-const pool = new Pool({
-    ...dbConfig,
-    max: 10,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000,
-});
+const pool = new Pool(dbConfig);
 
 export default pool;
