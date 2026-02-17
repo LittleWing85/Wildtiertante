64d4e65c563a85aec766070c0043727241847a1d
Dein Name <mail@alexandra-jaeger.net>
2026-02-17 17:48:28 +0100

    Full stack refactoring: central error handler for providing information about errors to frontend adapted

--- server/middleware/error.js
+++ server/middleware/error.js
@@ -1,10 +1,13 @@
 // Central error handling
 
+import { isProd } from "../config/env.js";
+
 const centralErrorHandler = (error, request, response, next) => {
     console.error("Server error:", error);
     if (response.headersSent) return next(error);
     response.status(error.status || 500).json({
         error: error.message || "Internal Server Error",
+        ...(!isProd && { stack: error.stack }),
     });
 };
 //add error handling for database request failed - e.g. because connectionTimeoutMillis has been exceeded
