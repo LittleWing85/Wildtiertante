ebc0f5b33f1319d9bf2c63890e1ab2718b12a020
Dein Name <mail@alexandra-jaeger.net>
2026-02-20 16:43:19 +0100

    Full stack/rework: body size for requests limited

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ import centralErrorHandler from "./middleware/error.js";
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "1mb" }));
 
 app.use(sessionMiddleware());
 app.use("/api", refreshSession);
