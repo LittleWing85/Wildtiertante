465feb37070ab6531789e4c1b9a30a6849655f67
Dein Name <mail@alexandra-jaeger.net>
2026-02-25 13:49:28 +0100

    Full stack refactoring: error handling for 401 in backend finalized

--- server/features/authentication/middleware.js
+++ server/features/authentication/middleware.js
@@ -1,8 +1,18 @@
-const requireAuthentication = (request, response, next) => {
-    if (!request.session.user_id) {
-        return response.status(401).json({ error: "Not logged in" });
+import { wrap } from "../../middleware/wrap.js";
+import { findUserById } from "./db.js";
+import { AuthenticationError } from "../../errors/AuthenticationError.js";
+
+const requireAuthentication = wrap(async (request, response) => {
+    if (!request.session?.user_id) {
+        throw new AuthenticationError(
+            "Du bist nicht eingeloggt. Bitte melde dich an.",
+        );
     }
-    next();
-};
+    const userExists = await findUserById(request.session.user_id);
+    if (!userExists) {
+        request.session = null;
+        throw new AuthenticationError("Ungültige Session.");
+    }
+});
 
 export { requireAuthentication };
