e59cc5592fd621aebda97acae79405f3fe0f7d7d
Alex <mail@alexandra-jaeger.net>
2026-04-21 18:43:50 +0200

    UI improvement: client side password check added

--- client/src/utils/formCheck.js
+++ client/src/utils/formCheck.js
@@ -5,11 +5,16 @@ export function formCheck(form) {
     const errorMessages = {};
 
     for (const element of form.elements) {
+        if (element.name === "password" && element.value.length < 12) {
+            console.log("Formcheck.js");
+            errorMessages[element.id] =
+                "Das Passwort muss mindestens 12 Zeichen lang sein.";
+        }
         if (element.willValidate && !element.checkValidity()) {
             if (element.validity.valueMissing) {
-                errorMessages[element.name] = "Dieses Feld ist erforderlich.";
+                errorMessages[element.id] = "Dieses Feld ist erforderlich.";
             } else if (element.validity.typeMismatch) {
-                errorMessages[element.name] = "Bitte prüfe deine Eingabe.";
+                errorMessages[element.id] = "Bitte prüfe deine Eingabe.";
             }
         }
     }
